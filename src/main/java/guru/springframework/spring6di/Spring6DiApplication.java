f7a40daaf4cc8b21425d7ba278bbced50fd0a9fb
guyriddler <guyriddler@gmail.com>
2024-02-14 20:58:13 -0600

    Profile additions, Environment Services, Life Cycle Methods

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class Spring6DiApplication {
 
--- src/main/java/guru/springframework/spring6di/controllers/i18n/Myi18NController.java
+++ src/main/java/guru/springframework/spring6di/controllers/i18n/Myi18NController.java
@@ -1,15 +1,14 @@
-package guru.springframework.spring6di.controllers;
+package guru.springframework.spring6di.controllers.i18n;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ConstructorInjectedController {
-
+public class Myi18NController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService){
+    public Myi18NController(@Qualifier("i18NService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
